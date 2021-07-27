bee728b98b4b9f8cd4731ce5976dea4679159b1b
Anomalous Alpha <blackcarrera@msn.com>
2021-07-27 17:30:44 -0600

    finished the query resovler stuff for thoughts and users

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,10 +20,69 @@ const typeDefs = gql`
     username: String
   }
 
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
 // export the typeDefs
 module.exports = typeDefs;
+
+// Query samples
+// query {
+//   # get all users
+//   users {
+//     username
+//     email
+//   }
+
+//   # get a single user by username (use a username from your database)
+//   user(username: "Cordell51") {
+//     username
+//     email
+//     friendCount
+//     thoughts {
+//       thoughtText
+//     }
+//     friends {
+//       username
+//     }
+//   }
+
+//   # query all thoughts
+//   thoughts {
+//     _id
+//     username
+//     thoughtText
+//     reactions {
+//       _id
+//       createdAt
+//       username
+//       reactionBody
+//     }
+//   }
+
+//   # query a single thought (use an _id from a thought in your database)
+//   thought(_id: "60ff3ef98077b52cb8e0d59f") {
+//     _id
+//     username
+//     thoughtText
+//     createdAt
+//     reactions {
+//       username
+//       reactionBody
+//     }
+//   }
+// }
