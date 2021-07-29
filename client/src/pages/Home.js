e7f1c2e09db7d6dcd2bfbb79739092896c5ccaeb
Anomalous Alpha <blackcarrera@msn.com>
2021-07-28 18:06:25 -0600

    have the list of toughts rendering on page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS } from '../utils/queries';
+import ThoughtList from '../components/ThoughtList/ThoughtList.js';
 
 const Home = () => {
   // use useQuery hook to make a query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
   console.log(thoughts);
+  console.log(loading);
+  console.log(data);
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="some Feed for thought(s)..." />
+          )}
+        </div>
       </div>
     </main>
   );
