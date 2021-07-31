f06ee474b288d9eff6715a4b5452d027efb7c0ef
Anomalous Alpha <blackcarrera@msn.com>
2021-07-31 17:43:25 -0600

    added ability to add reactions

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -59,7 +59,7 @@ const Profile = () => {
         )}
         
       </div>
-
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
@@ -73,7 +73,7 @@ const Profile = () => {
           />
         </div>
       </div>
-      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
+      
     </div>
   );
 };
