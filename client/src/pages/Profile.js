9b5e188b650ee68cef504f6c842ff6ef64eb7b02
CastroOlympias <blackcarrera@msn.com>
2022-01-29 17:25:03 -0700

    in my trouble shooting for my TFC project, I ahve found that the server package json file, scripts watch, was incrrect and needs to be nodemon server.js, it was just nodemon

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -52,6 +52,7 @@ const Profile = props => {
 
   return (
     <div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
@@ -77,7 +78,7 @@ const Profile = props => {
           />
         </div>
       </div>
-      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
+      
     </div>
   );
 };
